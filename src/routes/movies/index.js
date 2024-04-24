ee7dffb2af2af5db1ff72530068e5780c3d1dc3e
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2024-04-24 15:31:47 +0200

    add routes

--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -5,6 +5,18 @@ module.exports = (app) => {
     app.get('/movies', moviesController.getAll)
     expressRouter.get('/movies', moviesController.getAll)
     app.use('/api/v1', expressRouter)
+
+    // Créer un nouveau film
+    app.post('/movies', moviesController.create);
+
+    // Récupérer un film par ID
+    app.get('/movies/:id', moviesController.getById);
+
+    // Mettre à jour un film par ID
+    app.put('/movies/:id', moviesController.update);
+
+    // Supprimer un film par ID
+    app.delete('/movies/:id', moviesController.delete);
 }
 
 
